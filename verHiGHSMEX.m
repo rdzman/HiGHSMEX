02f8b1dc35fdd9625f8a6a826c7d037368c34a6c
Savyasachi Singh <savyasachi@users.noreply.github.com>
2025-06-12 14:28:41 -0400

    Added versioning to HiGHSMEX

---
+++ verHiGHSMEX.m
@@ -0,0 +1,11 @@
+function ver = verHiGHSMEX()
+% Version string of the HiGHSMEX project.
+%
+% Author: Savyasachi Singh
+%
+% Covered by the MIT License (see LICENSE file for details).
+% See https://github.com/savyasachi/HiGHSMEX for more information.
+
+ver = "1.1.0";
+
+% EOF
