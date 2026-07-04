9bf062c9c9bdf99f30af23756ddb3fbb54f726e5
Ray Zimmerman <rz10@cornell.edu>
2026-07-04 15:04:38 -0700

    Add manually triggered workflow to build MEX files on GitHub runners.
    - Builds macOS and Windows versions.
    - Clean up make_highsmex() and make it a function that takes a HiGHS version number as input.
    - Add workflow and action files.

--- make_highsmex.m
+++ make_highsmex.m
@@ -1,54 +1,72 @@
-% This script builds the highsmex.cpp file
+function make_highsmex(highs_ver)
+% This function builds the MEX file.
 %
 % Author: Savyasachi Singh
+% Modified by: Ray Zimmerman
 %
 % Covered by the MIT License (see LICENSE file for details).
 % See https://github.com/savyasachi/HiGHSMEX for more information.
 
 %% Inputs
-
-% link static file distributed by the HiGHS which includes the HiPO solver
-if ispc
-    highsInstallDir = fullfile('.', 'highs-1.15.1-x86_64-windows-static-apache');
-elseif ismac
-    highsInstallDir = fullfile('..', 'highs-1.15.1-arm-apple-static-apache');
-else
-    disp('highsInstallDir variable not defined in make_highsmex.m');
+if nargin < 1
+    highs_ver = '1.15.1';
 end
-% highsInstallDir = fullfile('.', 'HiGHS-1.13.1', 'installcpp20'); % link static file built from source
 
-% Path to the HiGHS library include directory
+% Path to HiGHS official binary HiGHS distribution, including HiPO solver.
+% Extract archive beside HiGHSMEX repo.
+if ispc
+    arch = 'x86_64-windows';        % Windows
+elseif ismac
+    arch =  'arm-apple';            % macOX
+elseif isunix
+    arch =  'x86_64-linux-gnu';     % Linux
+end
+highsInstallDir = fullfile('..', ...
+    sprintf('highs-%s-%s-static-apache', highs_ver, arch));
+
+%% Paths to HiGHS include & lib dirs and HIGHSMEX source file
 highsIncludeDir = fullfile(highsInstallDir, 'include', 'highs');
-
-% Path to folder containing the HiGHS static library
-highsLibIncludeDir = fullfile(highsInstallDir, 'lib');
-
-% Path to the highsmex.cpp file
+highsLibDir = fullfile(highsInstallDir, 'lib');
 mexSrcFilePath = fullfile('.', 'highsmex.cpp');
 
-%% Build mex file
-
+%% Platform specific compiler, linker flags
 compilerInfo=mex.getCompilerConfigurations('C++', 'Selected');
 compilerVendor=lower(compilerInfo.Manufacturer);
 switch compilerVendor
     case 'microsoft'
-        compflags={ 'COMPFLAGS="$COMPFLAGS  /std:c++20  /W3 "' };
-
-    case 'gnu'
-        compflags={ 'CXXFLAGS=''$CXXFLAGS  -std=c++20  -Wall ''' };
-
+        compflags = {
+            '-lopenblas', '-v', ...
+            'COMPFLAGS="$COMPFLAGS /std:c++20  /W3 "', ...
+        };
     case 'apple'
-        compflags={ 'CXXFLAGS="$CXXFLAGS -std=c++20 -mmacosx-version-min=13.4 "', ...
-            'LDFLAGS="$LDFLAGS -mmacosx-version-min=13.4 "' };
+        compflags = { '-lz', '-v',
+            'CXXFLAGS="$CXXFLAGS -std=c++20 -mmacosx-version-min=13.4 "', ...
+            'LDFLAGS="$LDFLAGS -mmacosx-version-min=13.4 -Wl,-framework,Accelerate"', ...
+        };
+    case 'gnu'
+        compflags = {
+            '-lopenblas', '-lz', '-v', ...
+            'COMPFLAGS="$COMPFLAGS -std=c++20 -Wall"', ...
+            'LDFLAGS="$LDFLAGS -static-libstdc++ -static-libgcc"', ...
+        };
+end
+if vstr2num(highs_ver) > 1.015
+    compflags = {'-lhighs_extras', compflags{:}};
 end
 
-% mex(mexSrcFilePath, '-R2018a', sprintf('-I"%s"', highsIncludeDir), sprintf('-L"%s"', highsLibIncludeDir), '-lhighs', '-v', compflags{:})
-if ispc
-    mex(mexSrcFilePath, '-R2018a', sprintf('-I"%s"', highsIncludeDir), sprintf('-L"%s"', highsLibIncludeDir), '-lhighs', '-lhighs_extras', '-lopenblas', '-v', compflags{:})
-elseif ismac
-    mex(mexSrcFilePath, '-R2018a', sprintf('-I"%s"', highsIncludeDir), sprintf('-L"%s"', highsLibIncludeDir), '-lhighs', '-lhighs_extras', '-lz', ['LDFLAGS=$LDFLAGS -Wl,-framework,Accelerate'], '-v', compflags{:})
-else
-    disp('Inputs to mex command not defined in make_highsmex.m');
-end
+%% Build mex file
+mex(mexSrcFilePath, '-R2018a', sprintf('-I"%s"', highsIncludeDir), ...
+    sprintf('-L"%s"', highsLibDir), '-lhighs', compflags{:});
 
-% EOF
+
+function num = vstr2num(vstr)
+% Converts version string to numerical value suitable for < or > comparisons
+% E.g. '1.15.1' -->  1.015001
+pat = '\.?(\d+)';
+[s,e,tE,m,t] = regexp(vstr, pat);
+b = 1;
+num = 0;
+for k = 1:length(t)
+    num = num + b * str2num(t{k}{1});
+    b = b / 1000;
+end
