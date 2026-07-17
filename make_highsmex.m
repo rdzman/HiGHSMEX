4380fb33c966b2d2a839570f87fd66fc4e1e0175
Ray Zimmerman <rz10@cornell.edu>
2026-07-17 10:37:48 -0700

    Debug workflow 11

--- make_highsmex.m
+++ make_highsmex.m
@@ -35,7 +35,8 @@ switch compilerVendor
         compflags={ 'COMPFLAGS="$COMPFLAGS  /std:c++20  /W3 "' };
 
     case 'gnu'
-        compflags={ 'CXXFLAGS=''$CXXFLAGS  -std=c++20  -Wall ''' };
+        compflags={ '-std=c++20', ''-Wall' };
+%        compflags={ 'CXXFLAGS=''$CXXFLAGS  -std=c++20  -Wall ''' };
 
     case 'apple'
         compflags={ 'CXXFLAGS="$CXXFLAGS -std=c++20 -mmacosx-version-min=13.4 "', ...
